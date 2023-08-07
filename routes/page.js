a01e095926a18ff140a9bf9c7acff406e2c7edd0
sujeong816 <sujeong1517@naver.com>
2023-08-07 22:05:44 +0900

    이미지 업로드 기능 추가

--- routes/page.js
+++ routes/page.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares')
+const { Post, User } = require('../models')
 
 const router = express.Router()
 
@@ -27,4 +28,22 @@ router.get('/', (req, res, next) => {
     })
 })
 
+router.get('/', async(req, res, next) => {
+    try {
+        const posts = await Post.findAll({
+            include: {
+                model: User,
+                attributes: ['id', 'nick']
+            }, 
+            order: [['createAt', 'DESC']]
+        })
+        res.render('main', {
+            title: 'NodeBird',
+            twits: posts
+        })
+    } catch (err) {
+        console.error(err)
+        next(err)
+    }
+})
 module.exports = router
